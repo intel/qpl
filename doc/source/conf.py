bc170648b066ee75c8d55f4268aabce8cb2103aa
Maria Zhukova <maria.zhukova@intel.com>
2023-10-25 10:50:36 -0700

    [fix][doc] Set 'navigation_with_keys' to False in conf.py to avoid Sphinx "default behavior changes" warning (#787)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,7 +71,8 @@ html_theme_options = {
     'use_issues_button': True,
     'use_edit_page_button': True,
     'repository_branch': 'develop',
-    'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>'
+    'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>',
+    'navigation_with_keys': False
 }
 
 # Capitalize d in "documentation"
