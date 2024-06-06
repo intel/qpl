f41309aedd420d365732437fc8cc8c9c02a8e1aa
Anthony Kung <anthony.kung@intel.com>
2024-06-06 10:36:17 -0700

    [test] Add new Scan test case for Force Array Output Modification (#961)

--- tools/tests/functional/algorithmic_tests/low_level_api/scan.cpp
+++ tools/tests/functional/algorithmic_tests/low_level_api/scan.cpp
@@ -13,6 +13,9 @@
 
 #include "qpl/qpl.h"
 
+// utils_common
+#include "iaa_features_checks.hpp"  // is_iaa_force_array_output_mod_supported()
+
 // common
 #include "analytic_fixture.hpp"
 #include "ta_ll_common.hpp"
@@ -417,6 +420,90 @@ namespace qpl::test
         EXPECT_TRUE(CompareTotalInOutWithReference());
         EXPECT_TRUE(compare_checksum_fields(job_ptr, reference_job_ptr));
         EXPECT_TRUE(CompareVectors(destination, reference_destination, job_ptr->total_out));
+    }
+
+    // Force Array Output Modification Test
+    // This test checks if the Force Array Output Modification is supported and works as expected.
+    // The test generates a source array with 64 elements, where each element is a value from a cycle of these values: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
+    // The test generates a reference value, which is a bitmask of the elements that should be in the scan range.
+    // The test performs a scan operation with the Force Array Output Modification enabled.
+    // The test checks if the output matches the reference value.
+    QPL_LOW_LEVEL_API_ALGORITHMIC_TEST_TC(scan, force_array_output_modification, ScanTest)
+    {
+        // Assert that Force Array Output Modification is supported
+        QPL_SKIP_TEST_FOR_EXPR_VERBOSE(is_iaa_force_array_output_mod_supported() == false, "Force array output modification not available on device, skipping test.");
+
+        // Skip test if on software path
+        QPL_SKIP_TEST_FOR_VERBOSE(qpl_path_software, "Force array output modification not available on software path");
+
+        constexpr const uint32_t source_size         = 64;                       // 64 elements
+        constexpr const uint32_t input_vector_width  = 8;                        // 8-bit values
+        constexpr const uint32_t output_vector_width = 32;                       // 32-bit values (because of Force Array Output Mod)
+        constexpr const uint32_t lower_boundary      = 7;                        // Lower boundary for scan range
+        constexpr const uint32_t upper_boundary      = 10;                       // Upper boundary for scan range
+        constexpr const uint32_t byte_bit_length     = 8;                        // 8 bits in a byte
+        constexpr const uint64_t reference           = 16045690984062438142ULL;  // Reference value for scan range
+
+        // Source and output containers
+        std::vector<uint8_t> source(source_size, 0);           // 64 bytes of data initialized with zeros
+        std::vector<uint8_t> destination(source_size * 4, 1);  // 256 bytes of data initialized with ones
+
+        std::unique_ptr<uint8_t[]> job_buffer;
+        uint32_t   size     = 0;
+        const auto *output = reinterpret_cast<const uint32_t *>(destination.data());
+
+        // Filling source containers
+        for (uint32_t i = 0; i < source_size; ++i) {
+            if ((1UL << i) & reference) {
+                source[i] = (i % 4) + 7;  // values in a cycle: 7, 8, 9, 10
+            }
+            else {
+                source[i] = (i % 7);  // values in a cycle: 0, 1, 2, 3, 4, 5, 6
+            }
+        }
+
+        // Job initialization
+        qpl_status status = qpl_get_job_size(qpl_path_hardware, &size);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job size getting.\n";
+
+        job_buffer = std::make_unique<uint8_t[]>(size);
+        qpl_job *job = reinterpret_cast<qpl_job *>(job_buffer.get());
+
+        status = qpl_init_job(qpl_path_hardware, job);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job initializing.\n";
+
+        // Performing an operation
+        job->next_in_ptr        = source.data();
+        job->available_in       = static_cast<uint32_t>(source.size());
+        job->next_out_ptr       = destination.data();
+        job->available_out      = static_cast<uint32_t>(destination.size());
+        job->op                 = qpl_op_scan_range;
+        job->src1_bit_width     = input_vector_width;
+        job->num_input_elements = static_cast<uint32_t>(source.size());
+        job->out_bit_width      = qpl_ow_32;  // without Force Array Output Mod, this will causes scan to return an array of indices
+        job->param_low          = lower_boundary;
+        job->param_high         = upper_boundary;
+
+        // Enable Force Array Output Modification
+        job->flags             |= QPL_FLAG_FORCE_ARRAY_OUTPUT;
+
+        status = run_job_api(job);
+
+        const auto bitmask_size_in_bytes    = job->total_out;
+        const auto bitmask_size_in_elements = bitmask_size_in_bytes * byte_bit_length / output_vector_width;
+
+        // Check if bitmask size matches the expected size
+        EXPECT_EQ(bitmask_size_in_elements, source_size) << "Bitmask size does not match the expected size.\n";
+
+        // Freeing resources
+        status = qpl_fini_job(job);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job finalizing.\n";
+
+        // Check if output matches the reference
+        for (uint32_t i = 0; i < source_size; ++i) {
+            // If the value should be in the scan range and it is not, return an error
+            ASSERT_EQ((1UL << i) & reference ? 1U : 0U, output[i]) << "Output value at index " << i << " does not match the expected value.\n";
+        }
     }
 
 #if defined(__linux__)
