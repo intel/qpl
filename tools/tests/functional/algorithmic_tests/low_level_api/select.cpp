9537415cac82f15a751c7c527d867c364f280645
Anthony Kung <anthony.kung@intel.com>
2024-06-06 10:35:47 -0700

    [test] Add new Select test case for Force Array Output Modification (#962)

--- tools/tests/functional/algorithmic_tests/low_level_api/select.cpp
+++ tools/tests/functional/algorithmic_tests/low_level_api/select.cpp
@@ -13,6 +13,9 @@
 
 #include "qpl/qpl.h"
 
+// utils_common
+#include "iaa_features_checks.hpp"  // is_iaa_force_array_output_mod_supported()
+
 // tests_common
 #include "analytic_mask_fixture.hpp"
 
@@ -120,6 +123,102 @@ namespace qpl::test
         ASSERT_EQ(QPL_STS_OK, reference_status);
 
         EXPECT_TRUE(CompareVectors(destination, reference_destination));
+    }
+
+    // Force Array Output Modification Check For Select
+    // This test checks if the Force Array Output Modification is supported for the Select operation.
+    // This test creates a source array with 64 elements and a mask
+    // The select operation is performed on the source array with the mask
+    // The output is an array with values from the source array where the mask is set
+    QPL_LOW_LEVEL_API_ALGORITHMIC_TEST_TC(select, force_array_output_modification, SelectTest)
+    {
+        // Assert that Force Array Output Modification is supported
+        QPL_SKIP_TEST_FOR_EXPR_VERBOSE(is_iaa_force_array_output_mod_supported() == false, "Force array output modification not available on device, skipping test.");
+
+        // Skip test if on software path
+        QPL_SKIP_TEST_FOR_VERBOSE(qpl_path_software, "Force array output modification not available on software path");
+
+        constexpr const uint32_t source_size                = 64;                  // Upper boundary for select
+        constexpr const uint32_t input_vector_width         = 8;
+        constexpr const uint32_t select_output_vector_width = 1;
+        constexpr const uint32_t byte_bit_length            = 8;
+        constexpr const uint64_t reference                  = 16045690984062438142ULL;  // Reference value for select
+
+        // Source and output containers
+        std::vector<uint8_t> source(source_size, 0);
+        std::vector<uint8_t> select_mask(source_size / 8, 4);
+        std::vector<uint8_t> destination(source_size * 4, 4);  // Destination are 32 bits which is 4 times larger than source
+
+        std::unique_ptr<uint8_t[]> job_buffer;
+        uint32_t   size = 0;
+
+        // Filling source containers
+        for (uint32_t i = 0; i < source_size; ++i) {
+            if ((1UL << i) & reference) {
+                source[i] = (i % 4) + 7;  // values in a cycle: 7, 8, 9, 10
+            }
+            else {
+                source[i] = (i % 7);  // values in a cycle: 0, 1, 2, 3, 4, 5, 6
+            }
+        }
+
+        // Fill mask after
+        select_mask[0] = 254;
+        select_mask[1] = 202;
+        select_mask[2] = 186;
+        select_mask[3] = 176;
+        select_mask[4] = 239;
+        select_mask[5] = 190;
+        select_mask[6] = 173;
+        select_mask[7] = 222;
+
+        // Job initialization
+        qpl_status status = qpl_get_job_size(qpl_path_hardware, &size);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job size getting.\n";
+        
+
+        job_buffer = std::make_unique<uint8_t[]>(size);
+        qpl_job *job = reinterpret_cast<qpl_job *>(job_buffer.get());
+
+        status = qpl_init_job(qpl_path_hardware, job);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job initializing.\n";
+
+        // Performing a select operation
+        job->next_in_ptr        = source.data();
+        job->available_in       = source_size;
+        job->next_out_ptr       = destination.data();
+        job->available_out      = static_cast<uint32_t>(destination.size());
+        job->op                 = qpl_op_select;
+        job->src1_bit_width     = input_vector_width;
+        job->num_input_elements = source_size;
+        job->out_bit_width      = qpl_ow_32;
+        job->next_src2_ptr      = select_mask.data();
+        job->available_src2     = 8;
+        job->src2_bit_width     = select_output_vector_width;
+
+        job->flags             |= QPL_FLAG_FORCE_ARRAY_OUTPUT;
+
+        status = run_job_api(job);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during performing select.\n";
+
+        const auto select_byte_size = job->total_out;
+
+        // Check if select size is 43 * 4
+        EXPECT_EQ(select_byte_size, 43 * 4);
+
+        // Freeing resources
+        status = qpl_fini_job(job);
+        ASSERT_EQ(QPL_STS_OK, status) << "An error " << status << " acquired during job finalization.\n";
+
+        // Compare with reference
+        uint32_t idx = 0;
+
+        for (uint32_t i = 0; i < source_size; ++i) {
+            if ((1UL << i) & reference) {
+                EXPECT_EQ(destination[idx], source[i]);
+                idx += 4;
+            }
+        }
     }
 
 #if defined(__linux__)
